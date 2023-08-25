8d1a7bff964e2245026707b36048ee54256efe7a
nyxtales0x0 <115275643+nyxtales0x0@users.noreply.github.com>
2023-08-25 21:02:23 +0530

    Update pre_chroot.sh
    enable pacman parallel downloads and color

--- pre_chroot.sh
+++ pre_chroot.sh
@@ -3,6 +3,10 @@
 # format the root partition
 yes | mkfs.ext4 /dev/nvme0n1p5
 
+# pacman configuration
+sed -i "s/#Color/Color/" /etc/pacman.conf
+sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 5/" /etc/pacman.conf
+
 # mount the root partition to /mnt
 mount /dev/nvme0n1p5 /mnt
 
@@ -19,4 +23,3 @@ arch-chroot /mnt ./post_chroot.sh
 
 # delete setup2.sh from /mnt
 arch-chroot /mnt rm ./post_chroot.sh
-
