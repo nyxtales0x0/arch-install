45ba7f7af4a47ddc1da01b41484d3f33742eff2b
nyxtales0x0 <birblikescoffee@gmail.com>
2024-03-24 12:38:18 +0530

    fixed tar extraction edstination

--- scripts/config_fonts.sh
+++ scripts/config_fonts.sh
@@ -7,7 +7,7 @@ function configure_fonts() {
     echo
 
     mkdir -p /usr/local/share/fonts
-    tar -xzf ./misc /usr/share/local/fonts/
+    tar -xzf ./misc -C /usr/share/local/fonts/
     mkdir ~/.config/fontconfig
     cp ./misc/fonts.conf ~/.config/fontconfig/
     fc-cache --force
