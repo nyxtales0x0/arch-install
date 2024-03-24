f0de9d03e4321a498b0a4c30259a7b1125f41900
nyxtales0x0 <birblikescoffee@gmail.com>
2024-03-24 12:21:04 +0530

    added fonts config

---
+++ scripts/config_fonts.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function configure_fonts() {
+    echo "=================="
+    echo "Configuring fonts:"
+    echo "=================="
+    echo
+
+    mkdir -p /usr/local/share/fonts
+    tar -xzf ./misc /usr/share/local/fonts/
+    mkdir ~/.config/fontconfig
+    cp ./misc/fonts.conf ~/.config/fontconfig/
+    fc-cache --force
+
+    echo "[OK]: Press enter to continue..."
+    read
+    clear
+}
+
+configure_fonts
